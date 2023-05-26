94ecedea5f035fb63c47db7a65a88f5d86fd1844
Clara Scherf <clara.scherf@gmail.com>
2023-05-26 16:07:44 +0200

    add HealthView change navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,37 +1,14 @@
 <template>
   <nav class="navbar navbar-expand-lg bg-body-tertiary">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">healthblog</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <a class="navbar-brand" href="#">My Healthblog</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-              Dropdown
-            </a>
-            <ul class="dropdown-menu">
-              <li><a class="dropdown-item" href="#">Action</a></li>
-              <li><a class="dropdown-item" href="#">Another action</a></li>
-              <li><hr class="dropdown-divider"></li>
-              <li><a class="dropdown-item" href="#">Something else here</a></li>
-            </ul>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled">About</a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
+        <router-link class="nav-link" to="/">Home </router-link>
+        <router-link class="nav-link" to="/about">About </router-link>
+        <router-link class="nav-link" to="/health">Health</router-link>
       </div>
     </div>
   </nav>
---
+++ src/views/HealthView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="health">
+    <h1>This is the health page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HealthView'
+}
+</script>
+
+<style scoped>
+
+</style>
