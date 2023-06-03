9c0761271abbc94a5e498ebc2972b34a4310291b
Deniz Ameti <deniz.ameti@student.htw-berlin.de>
2023-06-03 20:28:50 +0200

    fix navbar position

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,21 @@
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">My Healthblog</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <a class="navbar-brand" href="#">Health Blog App</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <router-link class="nav-link" to="/">Home </router-link>
-        <router-link class="nav-link" to="/about">About </router-link>
-        <router-link class="nav-link" to="/health">Health</router-link>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <router-link class="nav-link" to="/">Home</router-link>
+          <router-link class="nav-link" to="/about">About</router-link>
+          <router-link class="nav-link" to="/health">Health</router-link>
+        </div>
       </div>
     </div>
   </nav>
 </template>
-
 <script>
 export default {
   name: 'Navbar-component'
