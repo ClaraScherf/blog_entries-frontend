f57f574cad207354d476d580c5300a2dcba3f624
Clara Scherf <clara.scherf@gmail.com>
2023-06-23 21:09:58 +0200

    add text in AboutView.vue

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,24 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Was ist die Health Blog App ?</h1>
+    <p>Die Health Blog Webanwendung ist eine App, die es den Nutzern ermöglicht, die eigenen Gesundheitsdaten in einem Tagebuch zu erfassen und zu verfolgen.<br>
+      <br>
+      Folgende Features sind geplant: <br>
+      <br>
+      🏃 Schrittanzahl:<br>
+
+      Der Nutzer kann täglich seine Schrittanzahl eintragen, die er an einem Tag hinterlegt hat.<br>
+      <br>
+      🍎 Kalorienaufnahme:<br>
+
+      Der Nutzer kann die Kalorien, die er zu sich genommen hat, in der Anwendung eintragen und verfolgen.<br>
+      <br>
+      😃 Emojis: <br>
+
+      In der Anwendung kann der Nutzer Emojis (😄,😐,😔) eintragen, wie er seinen Tag bewertet bzw. wie sein Tag verlaufen ist.<br>
+      <br>
+      📓 Tagebuch-Eintrag: <br>
+
+      Der Nutzer kann eine Freifeldtexteingabe nutzen, um einige Worte über seinen Tag oder seine Gefühle zu erfassen.</p><br>
   </div>
 </template>
